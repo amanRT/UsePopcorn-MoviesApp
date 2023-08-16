6c90a147b43d3c7b0d6abdcd442a4a951b486f6f
Aman Thakur <amanrt360@gmail.com>
2023-08-16 20:20:30 +0530

    Star Component Complete

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={"55 "}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={2}
+    />
+    <StarRating maxRating={10} color="red " />
   </React.StrictMode>
 );
 
