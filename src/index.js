18c1d660a9460f019758c68c196260f02ab024c6
Aman Thakur <amanrt360@gmail.com>
2023-08-30 22:46:08 +0530

    File setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-import StarRating from "./StarRating";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-      defaultRating={2}
-    />
-    <StarRating maxRating={10} color="red " />
+    <App />
   </React.StrictMode>
 );
 
