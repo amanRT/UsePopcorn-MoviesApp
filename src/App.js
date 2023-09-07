7cc5fa8b7a23cf7b8dc2fdaba6b2d313b35e6d0e
Aman Thakur <amanrt360@gmail.com>
2023-09-07 23:47:59 +0530

    Added Docus

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -148,6 +148,10 @@ function Logo() {
   );
 }
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+  useEffect(function () {
+    inputEl.current.focus();
+  }, []);
   return (
     <input
       className="search"
@@ -155,6 +159,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
